f02b8e83ebcbcad9948b95b0eb4fa13b1e1b275c
NIIJIMA <7517650@ed.tus.ac.jp>
2018-06-03 15:00:46 +0000

    hogehoge2

--- unitest.py
+++ unitest.py
@@ -5,7 +5,7 @@ from plus import plus
 
 class TestPlus(unittest.TestCase):
     def test_init(self):
-        self.assertEqual(plus(1,2),3,"void int が間違ってます")
+        self.assertEqual(plus(1,2),3,"void int")
 
     def test_float(self):
         self.assertTrue(3.299999 < plus(1.1,2.2) < 3.300001, "error on float")
